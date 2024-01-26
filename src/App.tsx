061e8253941f724de7898f363bbc81e823edcdff
Fatima <fatimaraja1258@gmail.com>
2024-01-26 15:15:16 -0800

    Build sort selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
+import SortSelector from "./components/SortSelector";
 
 // query object 
 export interface GameQuery{
@@ -19,7 +20,7 @@ function App() {
     <Grid
       templateAreas={{
         base: `"nav" "main"`, // mobile -> single column
-        lg: `"nav nav " "aside main"`, // screen wider than 1024 px
+        lg: `"nav nav " "aside main"`, // screen wider than 1024 px 
       }}
       templateColumns={{
         base: "1fr",
@@ -40,9 +41,12 @@ function App() {
       </Show>
 
       <GridItem area="main">
-        <PlatformSelector selectedPlatform= {gameQuery.platform}
+      <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+      <PlatformSelector selectedPlatform= {gameQuery.platform}
           onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}
         />
+        <SortSelector/>
+      </HStack>
         <GameGrid gameQuery={gameQuery}/>
       </GridItem>
     </Grid>
---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,18 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { BsChevronDown } from 'react-icons/bs'
+
+const SortSelector = () => {
+  return (
+    <Menu>
+    <MenuButton as={Button} rightIcon={<BsChevronDown/>}>
+    
+    </MenuButton>
+    
+    <MenuList>
+       
+    </MenuList>
+        </Menu>
+  )
+}
+
+export default SortSelector
